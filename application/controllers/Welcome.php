bb297681f23ef9cf1faa43a47f687ebf37929b2a
Jonas Heschl <jonas@passgenau.digital>
2024-12-16 13:57:36 +0100

    URL encode special characters in example

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	public function index()
 	{
-		// Trigger the XSS by requesting http://localhost/?page=<script>alert();</script>
+		// Trigger the XSS by requesting http://localhost/?page=%3Cscript%3Ealert()%3C/script%3E
 		$page = $this->input->get("page");
 		if (isset($page)) {
 			$this->load->view($page);
