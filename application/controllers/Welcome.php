ca5ebbe77a924111dc56d1dc856e16ce24cb5d3a
Jonas Heschl <jonas@passgenau.digital>
2024-12-16 13:14:50 +0100

    Move page value to variable

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,8 +5,9 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// Trigger the XSS by requesting http://localhost/?page=<script>alert();</script>
-		if (isset($_GET["page"])) {
-			$this->load->view($_GET["page"]);
+		$page = $_GET["page"];
+		if (isset($page)) {
+			$this->load->view($page);
 		}
 	}
 }
