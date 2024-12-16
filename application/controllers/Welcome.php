3e7230f42f21768a44a468fd301fe1776db88e6d
Jonas Heschl <jonas@passgenau.digital>
2024-12-16 13:16:04 +0100

    Retrieve query param via CodeIgniter built-in library

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// Trigger the XSS by requesting http://localhost/?page=<script>alert();</script>
-		$page = $_GET["page"];
+		$page = $this->input->get("page");
 		if (isset($page)) {
 			$this->load->view($page);
 		}
